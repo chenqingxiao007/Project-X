de52d5abeb291d8b1f739eba419ace069e0fe997
chenqingxiao <chenqingxiao007@iCloud.com>
2017-02-06 17:11:41 +0800

    init

---
+++ Project-X/Project-X/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project-X
+//
+//  Created by 陈擎霄 on 2017/2/6.
+//  Copyright © 2017年 陈擎霄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
