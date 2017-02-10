1adf22ef9a8a6a4a3e0619c67c48bdd04a0b9ecf
陈擎霄 <chenqingxiao@chenqingxiaodeMacBook-Pro.local>
2017-02-10 15:00:40 +0800

    feat：添加pch文件

---
+++ Project-X/Project-X/Classes/Common/Controller/ZQLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZQLoginViewController.h
+//  Project-X
+//
+//  Created by 陈擎霄 on 2017/2/10.
+//  Copyright © 2017年 陈擎霄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZQLoginViewController : UIViewController
+
+@end
---
+++ Project-X/Project-X/Classes/Common/Controller/ZQLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZQLoginViewController.m
+//  Project-X
+//
+//  Created by 陈擎霄 on 2017/2/10.
+//  Copyright © 2017年 陈擎霄. All rights reserved.
+//
+
+#import "ZQLoginViewController.h"
+
+@interface ZQLoginViewController ()
+
+@end
+
+@implementation ZQLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
