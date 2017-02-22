70ee71b563e4074f730df259c7a31719aed6d6b7
ZQQiOS <zqq0702@sina.com>
2017-02-22 09:55:14 +0800

    perf：ZQAccountTool添加removeAccount方法

--- Project-X/Project-X/Classes/Tools/ZQAccountTool.m
+++ Project-X/Project-X/Classes/Tools/ZQAccountTool.m
@@ -7,12 +7,13 @@
 //
 
 #import "ZQAccountTool.h"
-// 文件保存路径
+// 文件保存路径 在Documents目录下创建文件
 #define kFile [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0] stringByAppendingPathComponent:@"account.data"]
 @implementation ZQAccountTool
 - (id)init
 {
     if (self = [super init]) {
+        //解归档该路径下的文件
         _account = [NSKeyedUnarchiver unarchiveObjectWithFile:kFile];
     }
     return self;
@@ -32,7 +33,13 @@
 - (void)saveAccount:(ZQAccount *)account
 {
     _account = account;
+    //将数据归档,如果前面的文件不存在，就会在归档数据时创建
     [NSKeyedArchiver archiveRootObject:account toFile:kFile];
 }
-
+- (void)removeAccount
+{
+    if ([[NSFileManager defaultManager] fileExistsAtPath:kFile]) {
+        [[NSFileManager defaultManager]removeItemAtPath:kFile error:nil];
+    }
+}
 @end
