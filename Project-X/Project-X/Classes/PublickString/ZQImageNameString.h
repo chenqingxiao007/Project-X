30bf26ace0ef35fd1b2405de95a2d7022101835e
zqq702 <zqq702@sina.com>
2017-02-22 23:32:02 +0800

    feat：添加图片名称类ZQImageNameString

---
+++ Project-X/Project-X/Classes/PublickString/ZQImageNameString.h
@@ -0,0 +1,16 @@
+//
+//  ZQImageNameString.h
+//  Project-X
+//
+//  Created by ZQQ on 17/2/22.
+//  Copyright © 2017年 陈擎霄. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZQImageNameString : NSObject
+
+#pragma mark -- my模块
+//**新的好友*/
+extern NSString *const my_friend;
+@end
---
+++ Project-X/Project-X/Classes/PublickString/ZQImageNameString.m
@@ -0,0 +1,15 @@
+//
+//  ZQImageNameString.m
+//  Project-X
+//
+//  Created by ZQQ on 17/2/22.
+//  Copyright © 2017年 陈擎霄. All rights reserved.
+//
+
+#import "ZQImageNameString.h"
+
+@implementation ZQImageNameString
+
+NSString *const my_friend = @"my_friend";
+
+@end
