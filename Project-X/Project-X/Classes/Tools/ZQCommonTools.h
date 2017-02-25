5fc7b61fc0299c9bf2d33bcf95474fb29b4dcd90
zqq702 <zqq702@sina.com>
2017-02-26 00:19:26 +0800

    feat:添加工具类，添加MBProgressHUD

---
+++ Project-X/Project-X/Classes/Tools/ZQCommonTools.h
@@ -0,0 +1,13 @@
+//
+//  ZQCommonTools.h
+//  Project-X
+//
+//  Created by ZQQ on 17/2/26.
+//  Copyright © 2017年 陈擎霄. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZQCommonTools : NSObject
+
+@end
---
+++ Project-X/Project-X/Classes/Tools/ZQCommonTools.m
@@ -0,0 +1,13 @@
+//
+//  ZQCommonTools.m
+//  Project-X
+//
+//  Created by ZQQ on 17/2/26.
+//  Copyright © 2017年 陈擎霄. All rights reserved.
+//
+
+#import "ZQCommonTools.h"
+
+@implementation ZQCommonTools
+
+@end
