92fc2c9e9b16b178d67eeeb7a69f2329263359ee
陈擎霄 <chenqx2@lenovo.com>
2017-06-29 15:02:45 +0800

    feat：推送自动播放推送内容

--- Project-X/Voice/NotificationService.m
+++ Project-X/Voice/NotificationService.m
@@ -1,60 +1,108 @@
+//
+//  NotificationService.m
+//  NotificationModifyService
+//
+//  Created by 刘旦 on 20/05/2017.
+//  Copyright © 2017 danny. All rights reserved.
+//
+
 #import "NotificationService.h"
+#import<AVFoundation/AVFoundation.h>
+
 
 @interface NotificationService ()
 
 @property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
 @property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
-@property (nonatomic,strong) NSURLSessionDownloadTask *downLoadTask;//下载的task 用于取消
+@property (nonatomic,strong) NSURLSessionDownloadTask *downLoadTask;
+
+//负责播放
+@property (nonatomic, strong) AVSpeechSynthesizer *aVSpeechSynthesizer;
 
 @end
 
 @implementation NotificationService
 
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
-    
     self.contentHandler = contentHandler;
     self.bestAttemptContent = [request.content mutableCopy];
     
     // Modify the notification content here...
     // self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
     
-    //以上代码均有系统自动生成
+    // read content
+    [self readContent:self.bestAttemptContent.body];
     
-    //获取下载的资源地址 这里和服务端约定好即可
-    NSString *url = request.content.userInfo[@"attach"];
-    NSURL *nsurl = [NSURL URLWithString:url];
     
-    //开始下载
-    self.downLoadTask = [[NSURLSession sharedSession] downloadTaskWithURL:nsurl completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        
-        if (!error) {
-            
-            //将下载后的文件进行 移动到沙盒 切记这里的文件要及时清理
-            NSString *path = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:response.suggestedFilename];
-            [[NSFileManager defaultManager] moveItemAtURL:location toURL:[NSURL fileURLWithPath:path] error:nil];
-            
-            //为 Notification 增加 attachment
-            UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"attachment" URL:[NSURL fileURLWithPath:path] options:nil error:nil];
-            
-            
-            self.bestAttemptContent.attachments = @[attachment];
-        }
-        
-        //不管成功失败 返回结果
-        self.contentHandler(self.bestAttemptContent);
-        
-    }];
-    
-    [self.downLoadTask resume];
+    /** download images
+     NSString *url = request.content.userInfo[@"attach"];
+     NSURL *nsurl = [NSURL URLWithString:url];
+     
+     self.downLoadTask = [[NSURLSession sharedSession] downloadTaskWithURL:nsurl completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+     
+     if (!error) {
+     
+     NSString *path = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:response.suggestedFilename];
+     [[NSFileManager defaultManager] moveItemAtURL:location toURL:[NSURL fileURLWithPath:path] error:nil];
+     
+     UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"attachment" URL:[NSURL fileURLWithPath:path] options:nil error:nil];
+     
+     
+     self.bestAttemptContent.attachments = @[attachment];
+     }
+     
+     
+     self.contentHandler(self.bestAttemptContent);
+     
+     }];
+     
+     [self.downLoadTask resume];
+     **/
 }
+
+- (void)readContent:(NSString*)str{
+    //AVSpeechUtterance: 可以假想成要说的一段话
+    AVSpeechUtterance * aVSpeechUtterance = [[AVSpeechUtterance alloc] initWithString:str];
+    
+    aVSpeechUtterance.rate = AVSpeechUtteranceDefaultSpeechRate;
+    
+    //AVSpeechSynthesisVoice: 可以假想成人的声音
+    aVSpeechUtterance.voice =[AVSpeechSynthesisVoice voiceWithLanguage:@"zh-CN"];
+    
+    //发音
+    [self.aVSpeechSynthesizer speakUtterance:aVSpeechUtterance];
+    
+}
+- (void)stopRead{
+    
+    [self.aVSpeechSynthesizer stopSpeakingAtBoundary:AVSpeechBoundaryImmediate];
+    
+}
+
+
+- (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didFinishSpeechUtterance:(AVSpeechUtterance *)utterance;{
+    
+    NSLog(@"阅读完毕");
+    self.contentHandler(self.bestAttemptContent);
+}
+
 
 - (void)serviceExtensionTimeWillExpire {
     // Called just before the extension will be terminated by the system.
     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    [self stopRead];
     NSLog(@"cancel ");
-    //取消下载
+    
     [self.downLoadTask cancel];
     self.contentHandler(self.bestAttemptContent);
 }
 
+- (AVSpeechSynthesizer *)aVSpeechSynthesizer{
+    if (!_aVSpeechSynthesizer) {
+        _aVSpeechSynthesizer = [[AVSpeechSynthesizer alloc] init];
+        _aVSpeechSynthesizer.delegate = self;
+    }
+    return _aVSpeechSynthesizer;
+}
 @end
+
